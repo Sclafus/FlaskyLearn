018683b52a6fd1a1ee2086e0ad89a89b810c5e4b
Scla <alessandro.sclafani00@gmail.com>
2021-03-11 20:27:00 +0100

    added js, it does nothing lol

---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+let email = sessionStorage.getItem("email");
+if(email !== null){
+  // something wrong here...already...damn i'm rusty on js
+  // document.getElementById('nav-el1').innerHTML() = "Course";
+}
