f27bda54f73f4fdd4a568f5cf5ce64ac7744c819
Scla <alessandro.sclafani00@gmail.com>
2021-03-19 14:34:07 +0100

    moved videoFormats to .env, added DB checks for register, added extension checking for uploads

--- server.py
+++ server.py
@@ -7,167 +7,201 @@ import mariadb
 import os
 import re
 
-#loading .env file
+# loading .env file
 load_dotenv()
 secretKey = os.getenv("SECRET_KEY")
 
+# .env allocation
 dbUser = os.getenv("DB_USER")
 dbPassword = os.getenv("DB_PASSWORD")
 dbHost = os.getenv("DB_HOST")
 dbPort = int(os.getenv("DB_PORT"))
 dbSchema = os.getenv("DB_SCHEMA")
 uploadFolder = os.getenv("UPLOAD_FOLDER")
+videoFormats = os.getenv("VIDEO_FORMATS").split(', ')
 
-# TODO add to .env
-# ALLOWED_EXTENSIONS = {'webm', 'mkv', 'flv', 'avi', 'mov', 'wmv', 'mp4', 'm4v', '3gp'}
-
+# Flask inizialization
 app = Flask(__name__)
 app.secret_key = sha256_crypt.hash(secretKey)
 app.config['UPLOAD_FOLDER'] = uploadFolder
 
-def dbConnect():
-  '''
-  Returns connection to the local database object
-  '''
-  try:
-    conn = mariadb.connect(
-      user = dbUser,
-      password = dbPassword,
-      host = dbHost,
-      port = dbPort,
-      database = dbSchema
-    )
-    return conn
 
-  except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    raise Exception
+def dbConnect():
+    '''
+    Returns connection to the local database object
+    '''
+    try:
+        conn = mariadb.connect(
+            user=dbUser,
+            password=dbPassword,
+            host=dbHost,
+            port=dbPort,
+            database=dbSchema
+        )
+        return conn
+
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        raise Exception
+    return None
+
+
+def allowedFile(filename: str) -> bool:
+    '''
+    Checks if the file is currently being accepted to upload
+    '''
+    ext = filename.split(".")[-1]
+    if ext in videoFormats:
+        return True
+    return False
+
 
 @app.route("/")
 def home():
-  '''
-  Displays the homepage, stored in index.html
-  '''
-  # try:
-  #   user = session["email"]
+    '''
+    Renders the homepage template
+    '''
+    # try:
+    #   user = session["email"]
 
-  # except KeyError:
-  #   #TODO ew
-  #   pass
-  return render_template("index.html")
+    # except KeyError:
+    #   #TODO ew
+    #   pass
+    return render_template("index.html")
+
 
 @app.route("/login/", methods=["POST", "GET"])
 def login():
-  '''
-  Redirects to login.html 
-  Handles POST requests to login users
-  '''
-  if request.method == "POST":
+    '''
+    Redirects to login.html 
+    Handles POST requests to login users
+    '''
+    if request.method == "POST":
 
-    email = request.form["email"]
-    password = request.form["password"]
+        email = request.form["email"]
+        password = request.form["password"]
 
-    #handling unchecked "Remember me" option
-    try:
-      stayLogged = request.form["stayLogged"]
-      if stayLogged:
-        app.permanent_session_lifetime = timedelta(days=1000)
-    
-    except KeyError:
-      pass
-    
-    # Connection to db
-    db = dbConnect()
-    dbCurr = db.cursor()
+        # handling unchecked "Remember me" option
+        try:
+            stayLogged = request.form["stayLogged"]
+            if stayLogged:
+                app.permanent_session_lifetime = timedelta(days=1000)
 
-    hhmail = sha256_crypt.hash(sha256_crypt.hash(email))
-    hhpass = sha256_crypt.hash(sha256_crypt.hash(password))
+        except KeyError:
+            pass
 
-    # The database stores hash of hash of both email and password 
-    dbCurr.execute("SELECT Email, Password FROM Students WHERE Email=?", (hhmail,))
+        # Connection to db
+        db = dbConnect()
+        dbCurr = db.cursor()
 
-    print(f"H(H(mail)): {hhmail}")
+        hhmail = sha256_crypt.hash(sha256_crypt.hash(email))
+        hhpass = sha256_crypt.hash(sha256_crypt.hash(password))
 
-    # TODO ew fix this
-    for (mail, passwd) in dbCurr:
-      if mail == hhmail:
-        if passwd == hhpass:
-          session["email"] = mail
-          print("epico")
-        else:
-          print("non troppo epico, ma quasi")
-      else:
-        print("Ao scemo registrate")
+        # The database stores hash of hash of both email and password
+        dbCurr.execute(
+            "SELECT Email, Password FROM Students WHERE Email=?", (hhmail,))
 
-    db.close()
-    #storing hash of hash of password and email in session for improved security || To implement
-    return redirect(url_for("home"))
+        # print(f"H(H(mail)): {hhmail}")
 
-  return render_template("login.html")
+        # TODO ew fix this
+        for (mail, passwd) in dbCurr:
+            if mail == hhmail:
+                if passwd == hhpass:
+                    session["email"] = mail
+                    print("epico")
+                else:
+                    print("non troppo epico, ma quasi")
+            else:
+                print("Ao scemo registrate")
+
+        db.close()
+        # storing hash of hash of password and email in session for improved security || To implement
+        return redirect(url_for("home"))
+
+    return render_template("login.html")
+
 
 @app.route("/register/", methods=["POST", "GET"])
 def register():
-  '''
-  TODO
-  '''
+    '''
+    TODO
+    '''
 
-  if request.method == "POST":
-    #get data from form
-    name = request.form["name"]
-    surname = request.form["surname"]
-    email = request.form["email"]
-    password = request.form["password"]
+    if request.method == "POST":
+        # get data from form
+        name = request.form["name"]
+        surname = request.form["surname"]
+        email = request.form["email"]
+        password = request.form["password"]
 
-    #double hash password and mail
-    hhmail = sha256_crypt.hash(sha256_crypt.hash(email))
-    hhpass = sha256_crypt.hash(sha256_crypt.hash(password))
-    print(f"H(H(mailRegister)): {hhmail}")
-    # Connection to db
-    db = dbConnect()
-    dbCurr = db.cursor()
+        # double hash password and mail
+        hhmail = sha256_crypt.hash(sha256_crypt.hash(email))
+        hhpass = sha256_crypt.hash(sha256_crypt.hash(password))
+        print(f"H(H(mailRegister)): {hhmail}")
 
-    # Insert data to db
-    try:
-      dbCurr.execute("INSERT INTO Students (Email, Password, Name, Surname) VALUES (?, ?, ?, ?)", (hhmail, hhpass, name, surname))
-    except mariadb.Error as e:
-      db.close()
-      print(f"👿Something happended {e}")
+        # Connection to db
+        db = dbConnect()
+        dbCurr = db.cursor()
 
-    #commit changes to db
-    db.commit()
-    db.close()
-    return redirect(url_for("home"))
+        dbCurr.execute("SELECT Email FROM Students WHERE Email=?", (hhmail, ))
 
-  return render_template("register.html")
+        for _ in dbCurr:
+            alreadyRegistered = True
+
+        # Insert data to db
+        try:
+            if not alreadyRegistered:
+                dbCurr.execute(
+                    "INSERT INTO Students (Email, Password, Name, Surname) VALUES (?, ?, ?, ?)", (hhmail, hhpass, name, surname))
+        except mariadb.Error as e:
+            db.close()
+            print(f"👿Something happended {e}")
+
+        # commit changes to db
+        db.commit()
+        db.close()
+        return redirect(url_for("home"))
+
+    return render_template("register.html")
+
 
 @app.route("/logout/")
 def logout():
-  ''' 
-  Deletes all the session data
-  '''
-  session.clear()
-  return redirect(url_for("home"))
+    ''' 
+    Deletes all the session data
+    '''
+    session.clear()
+    return redirect(url_for("home"))
+
 
 @app.route("/dashboard/", methods=['POST', 'GET'])
 def dashboard():
 
-  #TODO check on extensions
-  #TODO fix file select
-  #TODO add progress bar
-  if request.method == 'POST':
-    if 'video' not in request.files:
-      print('No file part')
-      return redirect(request.url)
-    file = request.files['video']
+    # TODO check permission!
+    # TODO fix file select
+    # POST request in this page uploads a new video
+    if request.method == 'POST':
+        #
+        if 'video' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['video']
 
-    if file.filename == '':
-      print('No selected file')
-      return redirect(request.url)
+        # handles unselected file
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
 
-    if file:
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  return render_template("dashboard.html")
+    # TODO add progress bar
+        if file and allowedFile(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        else:
+            print("Non accepted extension")
+
+    # TODO add new video to DB
+    return render_template("dashboard.html")
+
 
 if __name__ == '__main__':
-  app.run(threaded=True)
+    app.run(threaded=True)
