c2083f575bb42dd0b51807ae34246372e9b1b4a1
Scla <alessandro.sclafani00@gmail.com>
2021-04-24 17:41:37 +0200

    spacing

--- createDB.sql
+++ createDB.sql
@@ -8,48 +8,48 @@ CREATE TABLE IF NOT EXISTS `Contributor` (
   `password` varchar(64) DEFAULT null
 );
 
-CREATE TABLE IF NOT EXISTS  `Video` (
+CREATE TABLE IF NOT EXISTS `Video` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `description` text DEFAULT null,
   `path` varchar(255) DEFAULT null
 );
 
-CREATE TABLE IF NOT EXISTS  `Course` (
+CREATE TABLE IF NOT EXISTS `Course` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) DEFAULT null,
   `description` text DEFAULT null,
   `duration` int  UNSIGNED DEFAULT null
 );
 
-CREATE TABLE IF NOT EXISTS  `Student` (
+CREATE TABLE IF NOT EXISTS `Student` (
   `email` varchar(64) PRIMARY KEY,
   `name` varchar(255) DEFAULT null,
   `surname` varchar(255) DEFAULT null,
   `password` varchar(64) DEFAULT null
 );
 
-CREATE TABLE IF NOT EXISTS  `Release` (
+CREATE TABLE IF NOT EXISTS `Release` (
   `email` varchar(64),
   `id` int, 
   `timestamp` timestamp, 
   PRIMARY KEY (`email`, `id`)
 );
 
-CREATE TABLE IF NOT EXISTS  `Composition` (
+CREATE TABLE IF NOT EXISTS `Composition` (
   `videoid` int,
   `courseid` int,
   `lesson` int DEFAULT null,
   PRIMARY KEY (`videoid`, `courseid`)
 );
 
-CREATE TABLE IF NOT EXISTS  `Enrollment` (
+CREATE TABLE IF NOT EXISTS `Enrollment` (
   `email` varchar(64),
   `id` int,
   `timestamp` timestamp,
   PRIMARY KEY (`email`, `id`)
 );
 
-CREATE TABLE IF NOT EXISTS  `Visualization` (
+CREATE TABLE IF NOT EXISTS `Visualization` (
   `email` varchar(64),
   `id` int,
   `timestamp` timestamp,
@@ -57,17 +57,10 @@ CREATE TABLE IF NOT EXISTS  `Visualization` (
 );
 
 ALTER TABLE `Release` ADD FOREIGN KEY (`email`) REFERENCES `Contributor` (`email`);
-
 ALTER TABLE `Release` ADD FOREIGN KEY (`id`) REFERENCES `Video` (`id`);
-
 ALTER TABLE `Composition` ADD FOREIGN KEY (`videoid`) REFERENCES `Video` (`id`);
-
 ALTER TABLE `Composition` ADD FOREIGN KEY (`courseid`) REFERENCES `Course` (`id`);
-
 ALTER TABLE `Enrollment` ADD FOREIGN KEY (`email`) REFERENCES `Student` (`email`);
-
 ALTER TABLE `Enrollment` ADD FOREIGN KEY (`id`) REFERENCES `Course` (`id`);
-
 ALTER TABLE `Visualization` ADD FOREIGN KEY (`email`) REFERENCES `Student` (`email`);
-
 ALTER TABLE `Visualization` ADD FOREIGN KEY (`id`) REFERENCES `Video` (`id`);
