29a9add4469d75d70a65a9d01e5ecaea8d0fec5a
Scla <alessandro.sclafani00@gmail.com>
2021-04-25 22:59:16 +0200

    lessons are now working!

--- courses/courses.py
+++ courses/courses.py
@@ -1,20 +1,20 @@
 from flask import Blueprint, render_template, request
 from utils import Utils
 from dotenv import load_dotenv
-import os
+from os import getenv
 courses = Blueprint("courses", __name__,
                     static_folder='static', template_folder='templates')
 
 # .env allocation
 load_dotenv()
 env = {
-    'dbUser': os.getenv('DB_USER'),
-    'dbPassword': os.getenv('DB_PASSWORD'),
-    'dbHost': os.getenv('DB_HOST'),
-    'dbPort': int(os.getenv('DB_PORT')),
-    'dbSchema': os.getenv('DB_SCHEMA'),
-    'uploadFolder': os.getenv('UPLOAD_FOLDER'),
-    'videoFormats': os.getenv('VIDEO_FORMATS').split(', '),
+    'dbUser': getenv('DB_USER'),
+    'dbPassword': getenv('DB_PASSWORD'),
+    'dbHost': getenv('DB_HOST'),
+    'dbPort': int(getenv('DB_PORT')),
+    'dbSchema': getenv('DB_SCHEMA'),
+    'uploadFolder': getenv('UPLOAD_FOLDER'),
+    'videoFormats': getenv('VIDEO_FORMATS').split(', '),
 }
 
 utils = Utils(env)
@@ -32,7 +32,7 @@ def homepage():
     dbCurr.execute("SELECT * FROM Course")
     for courseid, name, duration, description in dbCurr:
         courses.append((courseid, name, duration, description))
-    return render_template('courses.html', context=courses)
+    return render_template('courses/courses.html', context=courses)
 
 
 @courses.route('/<int:courseId>/', methods=['POST', 'GET'])
@@ -49,8 +49,28 @@ def specificCourse(courseId: int):
         print(elem)
         videos.append(elem)
 
-    return render_template('course.html', videos=videos, courseId=courseId)
+    return render_template('courses/course.html', videos=videos, courseId=courseId)
 
-@courses.route('/<int:courseId>/lesson=<int:lessonId>', methods=['POST', 'GET'])
+
+@courses.route('/<int:courseId>/lesson<int:lessonId>', methods=['POST', 'GET'])
 def specificLesson(courseId: int, lessonId: int):
-    pass
+    '''Page for a specified lesson, displays video'''
+
+    dbCurr = db.cursor()
+
+    # getting course name
+    dbCurr.execute("SELECT name FROM Course WHERE id=?", (courseId, ))
+    for _courseName in dbCurr:
+        courseName = _courseName[0]
+
+    # getting video path
+    dbCurr.execute(
+        "SELECT path FROM Composition INNER JOIN Video on Composition.videoid = Video.id WHERE videoid = ? AND courseid=?", (lessonId, courseId))
+
+    for _path in dbCurr:
+        path = _path[0].split('/')
+        videoPath = '/'.join(path[2:])
+        folderPath = path[1]
+        print(f"video path: {videoPath}\n folder path: {folderPath}")
+
+    return render_template('courses/lesson.html', courseName=courseName, lessonId=lessonId, videoPath=videoPath, folderPath=folderPath)
