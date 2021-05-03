fbdb0ea8614b1bea6cd5052801d27169cd462638
Scla <alessandro.sclafani00@gmail.com>
2021-05-03 21:51:33 +0200

    added new buttons and fixed weird css stuff

--- flaskylearn/static/js/newQuiz.js
+++ flaskylearn/static/js/newQuiz.js
@@ -2,34 +2,49 @@
 let inputIndex = 0;
 
 /**
- * Adds a question to "quiz" div
+ * Adds a question to 'quiz' div
  */
 function addQuestion() {
     //linebreak object
-    let linebreak = document.createElement("br");
+    let linebreak = document.createElement('br');
 
     // creating new input field
-    let tag = document.createElement("input");
-    tag.type = "text";
-    tag.placeholder = "Insert question";
-    tag.id = ++inputIndex;
+    let newQuestion = document.createElement('input');
+    newQuestion.type = 'text';
+    newQuestion.placeholder = 'Insert question';
+    // newQuestion.classList.add('form-control');
+    newQuestion.id = ++inputIndex;
+    newQuestion.required = true;
 
-    // creating the button to add a new 
-    let newQuestionButton = document.createElement("button");
-    newQuestionButton.type = "button";
-    newQuestionButton.onclick = addResponseButton;
-    newQuestionButton.innerText = "➕";
+    // creating the button to add a new response 
+    let newAnswerButton = document.createElement('button');
+    newAnswerButton.type = 'button';
+    newAnswerButton.classList.add('btn', 'btn-primary');
+    newAnswerButton.onclick = addAnswer;
+    newAnswerButton.innerText = '➕';
 
-    // adding the input tag to 
-    let element = document.getElementById("quiz");
-    element.appendChild(tag);
-    element.appendChild(newQuestionButton);
-    element.appendChild(linebreak);
+    // creating the button to add a new response 
+    let deleteAnswerButton = document.createElement('button');
+    deleteAnswerButton.type = 'button';
+    deleteAnswerButton.classList.add('btn', 'btn-primary');
+    deleteAnswerButton.onclick = deleteAnswer;
+    deleteAnswerButton.innerText = '🗑️';
+    
+    // adding everything to the page
+    let quiz = document.getElementById('quiz');
+    quiz.append(newQuestion, newAnswerButton, deleteAnswerButton, linebreak);
 }
 
 /**
  * 
  */
-function addResponseButton(){
-    console.log("not doing anything yet");
+function addAnswer(){
+    console.log('not doing anything yet');
+}
+
+/**
+ * 
+ */
+function deleteAnswer(){
+    console.log('not doing anything yet again');
 }
