1dc3411b0e170ae882189e4941db73f640841fc4
Scla <alessandro.sclafani00@gmail.com>
2021-05-08 16:32:14 +0200

    form validation

--- flaskylearn/static/js/newQuiz.js
+++ flaskylearn/static/js/newQuiz.js
@@ -92,10 +92,73 @@ function deleteAnswer() {
 }
 
 /**
+ * 
+ * @returns formValidationError function.
+ * @see formValidationError
+ */
+function isFormValid() {
+    // getting the data
+    const questions = document.getElementById('quiz').children;
+
+    // checking that every question has at least two answers and at least one of them is correct
+    for (const question of questions) {
+        let flagCorrect = false;
+
+        // check if the question has a text
+        const questionText = question.children[0].value;
+        if (questionText === "") {
+            return formValidationError("A question has no text");
+        }
+
+        // filtering
+        const answers = Array.from(question.children).filter(ans => ans.classList.contains('answer'));
+
+        // check 1, at least two answers per question
+        if (answers.length <= 1) {
+            return formValidationError("A question needs at least two answers");
+        }
+
+        // looping over the answers
+        for (const answer of answers) {
+
+            let answerContainer = answer.children;
+
+            // check 2, answer text has been provided
+            if (answerContainer[0].value === "") {
+                return formValidationError("An answer is blank");
+            }
+
+            // check 3, at least one answer is correct
+            if (answerContainer[2].checked) {
+                flagCorrect = true;
+            }
+        }
+        if (!flagCorrect) {
+            return formValidationError("At least one answer needs to be correct");
+        }
+    }
+}
+
+/**
+ * Error function for form validation
+ * @param {string} error that caused the form to be invalid
+ * @returns false
+ */
+function formValidationError(error) {
+    // let body = document.getElementsByTagName('BODY')[0];
+    console.error(error);
+    return false;
+}
+
+/**
  * submits the for in a json object
  */
 function submitForm() {
-    // TODO form validation  
+
+    // form validation
+    if (!isFormValid()) {
+        return;
+    }
 
     // getting the data from the form
     const questions = document.getElementsByClassName('question');
@@ -123,15 +186,15 @@ function submitForm() {
         cache: "no-cache",
         headers: new Headers({
             "content-type": "application/json"
-        }) 
+        })
     }) //getting the response back
         .then(function (response) {
             if (response.status !== 200) {
                 console.log(`Response status was ${response.status}`)
-                return ;
+                return;
             }
 
-            response.json().then( function (data){
+            response.json().then(function (data) {
                 console.log(data);
                 location.reload();
             })
